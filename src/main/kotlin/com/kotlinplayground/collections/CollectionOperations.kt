3aa77b000250d1052713dd74f159c6318247ebf2
Weble21 <89642807+Weble21@users.noreply.github.com>
2025-09-11 18:36:55 +0900

    flatMap

--- src/main/kotlin/com/kotlinplayground/collections/CollectionOperations.kt
+++ src/main/kotlin/com/kotlinplayground/collections/CollectionOperations.kt
@@ -2,6 +2,7 @@ package com.kotlinplayground.collections
 
 import com.kotlinplayground.dataset.Course
 import com.kotlinplayground.dataset.CourseCategory
+import com.kotlinplayground.dataset.KAFKA
 import com.kotlinplayground.dataset.courseList
 
 fun main() {
@@ -11,9 +12,40 @@ fun main() {
     val devPredicate = { c : Course -> c.category == CourseCategory.DEVELOPEMENT }
     val desPredicate = { c : Course -> c.category == CourseCategory.DESIGN }
 
-    exploreFilter(courseList, desPredicate)
-    println("\n")
-    exploreMap(courseList, devPredicate)
+    val list = listOf(listOf(1,2,3), listOf(4,5,6))
+
+    val mapResult = list.map{ outerList ->
+        outerList.map {
+            it.toDouble()
+        }
+    }
+
+    println("mapResult = $mapResult")
+    val flatMapResult = list.flatMap{ outerList ->
+        outerList.map {
+            it.toDouble()
+        }
+    }
+    println("flatMapResult = $flatMapResult")
+
+
+    //exploreFilter(courseList, desPredicate)
+    //println("\n")
+    //exploreMap(courseList, devPredicate)
+    val courses = exploreFlatMap(courseList, KAFKA)
+    println("courses = $courses")
+}
+
+fun exploreFlatMap(courseList: MutableList<Course>, kafka: String): List<String> {
+    val kafkaCourses = courseList.flatMap { course ->
+        val courseName = course.name
+        course.topicsCovered.filter {
+            it == kafka
+        }.map {
+            courseName
+        }
+    }
+    return kafkaCourses
 }
 
 fun exploreMap(courseList: MutableList<Course>, predicate: (Course) -> Boolean) {
