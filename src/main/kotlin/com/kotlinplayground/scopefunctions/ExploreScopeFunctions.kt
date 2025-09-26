ebc3ee2539a219e951b1f031944e5c6396cef01e
Weble21 <89642807+Weble21@users.noreply.github.com>
2025-09-26 22:29:48 +0900

    let, with, run scope function

--- src/main/kotlin/com/kotlinplayground/scopefunctions/ExploreScopeFunctions.kt
+++ src/main/kotlin/com/kotlinplayground/scopefunctions/ExploreScopeFunctions.kt
@@ -4,8 +4,57 @@ import com.kotlinplayground.classes.Course
 import com.kotlinplayground.classes.CourseCategory
 
 fun main() {
-    exploreApply()
-    exploreAlso()
+    //exploreApply()
+    //exploreAlso()
+    //exploreLet()
+    //exploreWith()
+    exploreRun()
+}
+
+fun exploreRun() {
+    var numbers : MutableList<Int>? = null
+    val result = numbers.run {
+        numbers =mutableListOf(1, 2, 3)
+        numbers?.sum()
+    }
+    println("Run Result is : $result")
+
+    val length = run {
+        val name = "Donggeon"
+        println(name)
+        name.length
+    }
+    println("Run length is : $length")
+}
+
+fun exploreWith() {
+    val numbers = mutableListOf(1, 2, 3, 4, 5)
+
+    val result = with(numbers) {
+        println("Size is $size")
+        val list = numbers.plus(6)
+        list.sum()
+    }
+
+    println("With result is : $result")
+}
+
+//람다값을 반환함
+fun exploreLet() {
+    val numbers = mutableListOf(1,2,3,4,5)
+    val result = numbers.map{it*2}.filter { it > 5}.let {
+        println(it)
+        it.sum()
+    }
+    println(result)
+
+    var name : String? = null
+    name = "donggeon"
+    val result1 = name?.let {
+        println(it)
+        it.uppercase()  //이 람다값을 반환함
+    }
+    println(result1)
 }
 
 //.apply : 객체를 반환, 주로 객체의 초기화나 속성설정 시 사용, 객체 상태 변경이 주 목적
